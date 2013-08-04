a38988d78b86de02b9d4c7b604836d4f586ae07a
mikekrieger <mlkrgr@gmail.com>
2013-08-04 15:36:29 -0400

    exercise54.py completed

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,37 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Michael Krieger
+
+
+#1
+import math
+
+def is_triangle(n):
+	if a>(b+c):
+		print 'No'
+	elif b>(a+c):
+		print 'No'
+	elif c>(a+b):
+		print 'No'
+	elif a=b+c
+		print 'degenerate'
+	elif b=a+c
+		print 'degenerate'
+	elif c=a+b
+		print 'degenerate'
+	else:
+		print 'Yes'
+
+#2
+var = raw_input('Enter length of stick a: ')
+print 'you entered ', var
+
+var = raw_input('Enter length of stick b: ')
+print 'you entered ', var
+
+var = raw_input('Enter length of stick c: ')
+print 'you entered ', var
+int(a)
+int(b)
+int(c)
+
+is_triangle(a,b,c)
