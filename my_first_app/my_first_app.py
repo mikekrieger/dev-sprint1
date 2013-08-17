27bb5508592c0fa5ab8a8033f418233f29c6767f
mikekrieger <mlkrgr@gmail.com>
2013-08-16 21:41:47 -0400

    myfirstapp

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -1,2 +1,14 @@
 # This is whre you can start you python file for your week1 web app
-# Name: 
+# Name: Michael Krieger
+
+import flask, flask.views
+app = flask.Flask(__name__)
+
+class View(flask.views.MethodView):
+	def get(self):
+		return "Hello world!"
+
+app.add_url_rule('/', view_func=View.as_view('main'))
+
+app.debug = True
+app.run()
